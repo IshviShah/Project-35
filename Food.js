31b2351501688f1611deb3cd7899c89bc4bb33ba
IshviShah <73232829+IshviShah@users.noreply.github.com>
2021-02-26 17:02:13 +0530

    Update Food.js

--- Food.js
+++ Food.js
@@ -1,15 +1,15 @@
 class Food {
     constructor(){
         this.image = loadImage("Milk.png");
-        this.foodStock=0;
+        this.foodStock = 0;
         this.lastFed;
     }
     display(){
         var x=80,y=100;
-        
+
         imageMode(CENTER);
         image(this.image,720,220,70,70);
-        
+
         if(this.foodStock!=0){
           for(var i=0;i<this.foodStock;i++){
             if(i%10==0){
@@ -22,14 +22,17 @@ class Food {
         }
       }
         getFoodStock(){
-
+          return this.foodStock;
         }
         updateFoodStock(foodStock){
-            this.foodStock=foodStock;
+            this.foodStock = foodStock;
         }
-        
         deductFood(){
-
+          
+         if(this.foodStock>0){
+           this.foodStock = this.foodStock-1;
+         }
+          
         }
 
 
